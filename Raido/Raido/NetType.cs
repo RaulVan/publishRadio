7db698ad2fbc8fcfa601467eb8b4c6412cd9feee
le.weng <le.weng@autonavi.com>
2014-05-18 11:52:33 +0800

    111111

---
+++ Raido/Raido/NetType.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Phone.Net.NetworkInformation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raido
+{
+    public class NetType
+    {
+
+        enum netType
+        {
+            _3G = 0,
+            _2G,
+            _WIFI,
+            _NoNet
+        }
+
+        public netType GetNetType()
+        {
+            if (Microsoft.Phone.Net.NetworkInformation.NetworkInterface.GetIsNetworkAvailable() == false)
+                return netType._NoNet;
+            else
+            {
+                var info = Microsoft.Phone.Net.NetworkInformation.NetworkInterface.NetworkInterfaceType;
+                switch (info)
+                {
+                    case NetworkInterfaceType.MobileBroadbandCdma:
+                        return netType._3G;
+                    case NetworkInterfaceType.MobileBroadbandGsm:
+                        return netType._2G;
+                    case NetworkInterfaceType.Wireless80211:
+                        return netType._WIFI;
+                    case NetworkInterfaceType.Ethernet:
+                        return "Ethernet";
+                    case NetworkInterfaceType.None:
+                        return "None";
+                    default:
+                        return "Other";
+                } 
+            }
+        }
+
+    }
+}
